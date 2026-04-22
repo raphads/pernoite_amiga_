01fcc991177042dcc4f48095e4376406aadf702e
raphads <raph.ads2019@gmail.com>
2026-04-22 03:04:18 -0300

    added functions and css adjusts

--- pernoite.js
+++ pernoite.js
@@ -5,5 +5,20 @@ function toggleMenu() {
    }
    if (document.getElementById("sidebar").classList.toggle("false")){
         document.getElementById("sidebar").classList.toggle("active");
+}
+}
+
+function horario(){
+     var hora = document.getElementById("horas").checked;
+     if(hora == true){
+          document.getElementById("tempo1").readOnly = true;
+          document.getElementById("tempo2").readOnly = true;
+          document.getElementById("tempo1").value = "00:00";
+          document.getElementById("tempo2").value = "00:00";
+          //document.getElementById("tempo").disabled = true;
+     } else{
+         document.getElementById("tempo1").readOnly = false;
+         document.getElementById("tempo2").readOnly = false;
+         //document.getElementById("tempo").disabled = false;
      }
 }
